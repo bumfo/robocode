1fe6b0caa06567209389833487dec310ab6a326a
bumfo <bumfo@users.noreply.github.com>
2018-11-07 15:11:41 +0800

    print code size error

--- robocode.api/src/main/java/net/sf/robocode/repository/CodeSizeCalculator.java
+++ robocode.api/src/main/java/net/sf/robocode/repository/CodeSizeCalculator.java
@@ -15,41 +15,36 @@ import java.lang.reflect.Method;
 
 /**
  * Tool used for calculating the code size of a directory or jar file.
- * 
+ *
  * @author Flemming N. Larsen (original)
  */
 public final class CodeSizeCalculator {
 
-	public static Integer getDirectoryCodeSize(File dir) {
-		return getCodeSize("processDirectory", dir);
-	}
+  public static Integer getDirectoryCodeSize(File dir) {
+    return getCodeSize("processDirectory", dir);
+  }
 
-	public static Integer getJarFileCodeSize(File jarFile) {
-		return getCodeSize("processZipFile", jarFile);
-	}
+  public static Integer getJarFileCodeSize(File jarFile) {
+    return getCodeSize("processZipFile", jarFile);
+  }
 
-	private static Integer getCodeSize(String invokeMethod, File jarFile) {
-		Integer codesize;
-		try {
-			// Call the code size utility using reflection
-			Class<?> classType = Class.forName("codesize.Codesize");
+  private static Integer getCodeSize(String invokeMethod, File jarFile) {
+    Integer codesize;
+    try {
+      // Call the code size utility using reflection
+      Class<?> classType = Class.forName("codesize.Codesize");
 
-			Method method = classType.getMethod(invokeMethod, new Class[] { File.class });
-			Object item = method.invoke(null/* static method */, jarFile);
+      Method method = classType.getMethod(invokeMethod, new Class[]{File.class});
+      Object item = method.invoke(null/* static method */, jarFile);
 
-			// Calls Codesize.Item.getCodeSize()
-			method = item.getClass().getMethod("getCodeSize", (Class[]) null);
-			codesize = (Integer) method.invoke(item, (Object[]) null);
+      // Calls Codesize.Item.getCodeSize()
+      method = item.getClass().getMethod("getCodeSize", (Class[]) null);
+      codesize = (Integer) method.invoke(item, (Object[]) null);
 
-		} catch (IllegalAccessException e) {
-			codesize = null;
-		} catch (InvocationTargetException e) {
-			codesize = null;
-		} catch (NoSuchMethodException e) {
-			codesize = null;
-		} catch (ClassNotFoundException e) {
-			codesize = null;
-		}
-		return codesize;
-	}
+    } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+      e.printStackTrace();
+      codesize = null;
+    }
+    return codesize;
+  }
 }
